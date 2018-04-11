aa4bbcb709d14dd7d11d99d9ed14dd0fdbc7eff4
Keith Peters <kp@bit-101.com>
2018-04-11 06:11:42 -0400

    tests

---
+++ bitmath/math_test.go
@@ -0,0 +1,32 @@
+package bitmath
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNorm(t *testing.T) {
+	if Norm(0, 0, 100) != 0 {
+		t.Error("Norm(0, 0, 100) != 0")
+	}
+	if Norm(100, 0, 100) != 1 {
+		t.Error("Norm(100, 0, 1) != 1")
+	}
+	if Norm(50, 0, 100) != 0.5 {
+		t.Error("Norm(50, 0, 100) != 0.5")
+	}
+	if Norm(-50, 0, 100) != -0.5 {
+		t.Error("Norm(-50, 0, 100) != -0.5")
+	}
+	if Norm(150, 0, 100) != 1.5 {
+		t.Error("Norm(150, 0, 100) != 1.5")
+	}
+	if Norm(0, 100, 0) != 1 {
+		t.Error("Norm(0, 100, 0) != 1")
+	}
+	fmt.Println(Norm(1, 100, 0))
+	if Norm(1, 100, 0) != 0 {
+		t.Error("Norm(1, 100, 0) != 0")
+	}
+
+}
