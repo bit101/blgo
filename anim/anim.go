0ef38f6cc530ed20bf0f27590097e2bcb51e0d3f
Keith Peters <kp@bit-101.com>
2018-04-30 21:48:28 -0400

    anim

--- anim/anim.go
+++ anim/anim.go
@@ -2,7 +2,9 @@ package anim
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 	"strconv"
 
@@ -12,18 +14,55 @@ import (
 // RenderCallback signature for the function that will be called each frame.
 type RenderCallback func(*bitlibgo.BitSurface, float64)
 
-// MakeGif makes a gif.
-func MakeGif(
-	surface *bitlibgo.BitSurface,
-	frameCount int,
-	fps int,
-	tempDir string,
-	outputFile string,
-	renderFunc RenderCallback,
-) {
-	framesSpec := fmt.Sprintf("%s/*.png", tempDir)
-	makeFrames(surface, frameCount, tempDir, renderFunc)
-	convertGif(fps, framesSpec, outputFile)
+// Animation is a animated gif maker.
+type Animation struct {
+	Width    float64
+	Height   float64
+	Frames   int
+	FPS      int
+	Filename string
+}
+
+// NewAnimation creates a new animated gif project.
+func NewAnimation(filename string) *Animation {
+	return &Animation{
+		Width:    200,
+		Height:   200,
+		Frames:   60,
+		FPS:      30,
+		Filename: filename,
+	}
+}
+
+// Render renders the gif
+func (p *Animation) Render(renderCallback RenderCallback) {
+
+	framesDir, err := ioutil.TempDir(".", "frames_")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	surface := bitlibgo.NewBitSurface(p.Width, p.Height)
+	makeFrames(surface, p.Frames, framesDir, renderCallback)
+
+	framesSpec := fmt.Sprintf("%s/*.png", framesDir)
+	convertGif(p.FPS, framesSpec, p.Filename)
+
+	os.RemoveAll(framesDir)
+}
+
+// SetSize sets the size
+func (p *Animation) SetSize(w float64, h float64) {
+	p.Width = w
+	p.Height = h
+}
+
+func makeFrames(surface *bitlibgo.BitSurface, numFrames int, framesDir string, renderFunc RenderCallback) {
+	for i := 0; i < numFrames; i++ {
+		renderFunc(surface, float64(i)/float64(numFrames))
+		filename := fmt.Sprintf("%s/anim_%0.3d.png", framesDir, i)
+		surface.WriteToPNG(filename)
+	}
 }
 
 // requires imagemagick to be installed.
@@ -34,14 +73,6 @@ func convertGif(fps int, input string, output string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("saved!")
+	fmt.Printf("Gif saved as %s\n", output)
 
-}
-
-func makeFrames(surface *bitlibgo.BitSurface, numFrames int, framesPath string, renderFunc RenderCallback) {
-	for i := 0; i < numFrames; i++ {
-		renderFunc(surface, float64(i)/float64(numFrames))
-		filename := fmt.Sprintf("%s/anim_%0.3d.png", framesPath, i)
-		surface.WriteToPNG(filename)
-	}
 }
